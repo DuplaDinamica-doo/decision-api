8086b6ab8f949d0e86c0555d79375f3bfccc14d6
Jaiane <jaianesoares700@gmail.com>
2025-09-08 22:46:44 -0300

    first commit

---
+++ src/main/java/DuplaDinamica_doo/CreditRisk_limite/CreditRiskLimiteApplication.java
@@ -0,0 +1,13 @@
+package DuplaDinamica_doo.CreditRisk_limite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditRiskLimiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditRiskLimiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DuplaDinamica_doo/CreditRisk_limite/CreditRiskLimiteApplicationTests.java
@@ -0,0 +1,13 @@
+package DuplaDinamica_doo.CreditRisk_limite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditRiskLimiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
